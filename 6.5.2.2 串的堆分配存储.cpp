4d2cc6b2ff1021cb5c03b71511a1046ab69bd68e
13636922130 <c872534679@qq.com>
2019-08-17 20:25:25 +0800

    添加更多的基本操作

--- 6.5.2.2 串的堆分配存储.cpp
+++ 6.5.2.2 串的堆分配存储.cpp
@@ -11,7 +11,7 @@ typedef struct
 void StrAssign(HString &s, const char *ch)
 {
     int len=0;
-    while(ch[len] != NULL)
+    while(ch[len])
         len++;
     s.data = (char *)malloc(len*sizeof(char));
     s.length = len;
@@ -35,6 +35,62 @@ bool StrInsert(HString &s, int pos, HString s1)
     return true;
 }
 
+//串比较操作
+int StrCompare(HString s1, HString s2)
+{
+    for(int i=0;i<s1.length&&i<s2.length;i++) //取s1 s2中长度的最小值
+    {
+        if(s1.data[i] < s2.data[i])
+            return -1;
+        else if(s1.data[i] > s2.data[i])
+            return 1;
+    }
+    if(s1.length == s2.length) return 0;
+    return (s1.length>s2.length?1:-1);
+}
+
+//求串长
+int StrLength(HString s)
+{
+    return s.length;
+}
+
+//清空串操作
+void ClearStr(HString &s)
+{
+    free(s.data);
+    s.length = 0;
+    s.data = NULL;
+}
+
+//串拼接操作
+void StrCat(HString &s, HString s1, HString s2)
+{
+    int len = s1.length + s2.length;
+    int i;
+    s.data = (char *)malloc(len*sizeof(char));
+    s.length = len;
+    for(i=0;i<s1.length;i++)
+        s.data[i] = s1.data[i];
+    for(int j=0;j<s2.length;j++)
+        s.data[i++] = s2.data[j];
+}
+
+//获取子串
+bool SubString(HString s, HString &sub, int pos, int l)
+{
+    if( pos<1 || pos>s.length || l < 0 || (pos+l)>s.length + 1)
+        return false;
+    if(sub.data) free(sub.data);
+    sub.data = (char *)malloc(l*sizeof(char));
+    int len = 0;
+    for(int i=0;i<l;i++)
+        sub.data[len++] = s.data[pos-1+i];
+    sub.length = len;
+    return true;
+}
+
+//输出串操作
 void PrintString(HString s)
 {
     for(int i=0;i<s.length;i++)
@@ -44,9 +100,14 @@ void PrintString(HString s)
 
 int main()
 {
-    HString s, s1;
-    StrAssign(s, "Hello, world");
-    StrAssign(s1, "the");
-    StrInsert(s, 13, s1);
+    HString s, s1, s2, sub;
+    StrAssign(s1, "Hello,");
+    StrAssign(s2, " world");
+    StrAssign(sub, "fuck");
+    StrCat(s, s1, s2);
     PrintString(s);
+    if(!SubString(s, sub, 9, 4))
+        printf("Error!\n");
+    else
+        PrintString(sub);
 }
