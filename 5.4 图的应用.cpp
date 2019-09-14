57a831977b432b5e33386fe125649e7ba90b7721
13636922130 <c872534679@qq.com>
2019-09-14 15:58:34 +0800

    添加Floyd算法并输出执行过程

--- 5.4 图的应用.cpp
+++ 5.4 图的应用.cpp
@@ -3,3 +3,65 @@
 而是考察算法具体执行过程
 具体源代码后期贴上
 */
+
+#include<cstdio>
+#define VEXMAX 1000
+//无穷大的定义参考 https://blog.csdn.net/mylovestart/article/details/8238088
+#define INF 0x3F3F3F3F
+int G[VEXMAX][VEXMAX];
+int dist[VEXMAX][VEXMAX];
+int n;
+
+void show()
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(dist[i][j] == INF)
+                printf("  -1");
+            else
+                printf("%4d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    printf("Enter the number of vertex:");
+    scanf("%d", &n);
+    printf("Enter the graph(-1 stands for infinite)\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d", &G[i][j]);
+            if(G[i][j] == -1)
+                G[i][j] = INF;
+            else if(i == j)
+                G[i][j] = 0;
+
+            dist[i][j] = G[i][j];
+        }
+    }
+
+    printf("******dist(-1)******\n");
+    show();
+    printf("********************\n");
+    for(int k=0;k<n;k++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(dist[i][j] > dist[i][k] + dist[k][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+
+        printf("******dist(%d)******\n", k);
+        show();
+        printf("********************\n");
+    }
+
+}
